ad2c4741a6608cd651053220f0e4ebe3294971c0
packjuvenile <3283890195@qq.com>
2026-02-02 13:17:44 +0800

    modified:   config/system.kbd 	modified:   kanata_run.bat

--- kanata_run.bat
+++ kanata_run.bat
@@ -1,1 +1,6 @@
-kanata_windows_tty_winIOv2_cmd_allowed_x64.exe --cfg config/system.kbd &
+@echo off
+cd /d "%~dp0"
+echo Set WshShell = CreateObject("WScript.Shell") > "%temp%\tmp_run.vbs"
+echo WshShell.Run "kanata_windows_tty_winIOv2_cmd_allowed_x64.exe --cfg config/system.kbd", 0, False >> "%temp%\tmp_run.vbs"
+cscript //nologo "%temp%\tmp_run.vbs"
+del /f /q "%temp%\tmp_run.vbs"
