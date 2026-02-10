73779b368434d83de4726c4fd8351cb1d0e8ea7b
packjuvenile <3283890195@qq.com>
2026-02-10 11:11:04 +0800

    modified:   kanata_run.bat

--- kanata_run.bat
+++ kanata_run.bat
@@ -1,8 +1,3 @@
 @echo off
 cd /d "%~dp0"
-
-echo Set WshShell = CreateObject("WScript.Shell") > "%temp%\kanata_run.vbs"
-echo WshShell.Run "kanata.exe --cfg config/system.kbd", 0, False >> "%temp%\kanata_run.vbs"
-
-cscript //nologo "%temp%\kanata_run.vbs"
-del /f /q "%temp%\kanata_run.vbs"
+kanata.exe --cfg config/system.kbd
